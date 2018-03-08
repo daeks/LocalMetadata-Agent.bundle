8648c3ccdb4b5b85a211c6738921cdf325544f80
daeks <daeks@gmx.de>
2018-03-08 11:21:01 +0100

    added art

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -7,7 +7,7 @@ def Start():
   
 def ValidatePrefs():
   pass
-
+    
 class LocalMetadataAgent(Agent.TV_Shows):
   name = 'Local Metadata Agent (TV)'
   languages = [Locale.Language.NoLanguage]
@@ -33,23 +33,33 @@ class LocalMetadataAgent(Agent.TV_Shows):
             for poster in json_object[key]:
               if poster not in metadata.posters:
                 metadata.posters[poster] = Proxy.Media(Core.storage.load(poster), sort_order=1)
+          elif key == 'art':
+            for art in json_object[key]:
+              if art not in metadata.art:
+                metadata.art[art] = Proxy.Media(Core.storage.load(art), sort_order=1)
           elif key == 'seasons':
             for snum, season in media.seasons.iteritems():
-              #Log('[METADATA] Parsing season %s' % season.index)
+              Log('[METADATA] Parsing season %s' % season.index)
               if ('%02d' % int(season.index)) in json_object[key]:
                 season_json_object = json_object[key][('%02d' % int(season.index))]
+                Log('[METADATA] Found season %s - %s' % (season.index, season_json_object))
                 for skey, svalue in season_json_object.iteritems():
                   if skey == 'posters':
                     for poster in season_json_object[key]:
                       if poster not in season.posters:
                         season.posters[poster] = Proxy.Media(Core.storage.load(poster), sort_order=1)
+                  elif key == 'art':
+                    for art in season_json_object[key]:
+                      if art not in season.art:
+                        season.art[art] = Proxy.Media(Core.storage.load(art), sort_order=1)
                   elif skey == 'episodes':
                     for enum, episode in season.episodes.iteritems():
-                      #Log('[METADATA] Parsing season %s - episode %s' % (season.index, episode.index))
+                      Log('[METADATA] Parsing season %s - episode %s' % (season.index, episode.index))
                       if ('%02d' % int(episode.index)) in season_json_object[skey]:
                         episode_json_object = season_json_object[skey][('%02d' % int(episode.index))]
+                        Log('[METADATA] Found season %s - episode %s - %s' % (season.index, episode.index, episode_json_object))
                         for ekey, evalue in episode_json_object.iteritems():
-                          setattribute(self, metadata.seasons[season.index].episodes[episode.index], ekey, evalue)
+                          setattribute(self, metadata.seasons[season.index].episodes[episode.index], ekey, evalue) 
                   else:
                     setattribute(self, metadata.seasons[season.index], skey, svalue)
           else:
